d180a04e6b7d25624cbc4281c2f44826957c138e
todddrinkwater <todddrinkwaternz@gmail.com>
2017-11-01 10:11:43 +1300

    remove whitespace

--- lib/console_ui.rb
+++ lib/console_ui.rb
@@ -8,52 +8,52 @@ class ConsoleUI
     @word = ["horse", "dog", "flux", "rails"].sample
     @game = Game.new(lives_remaining: 7, guess_word: word)
   end
-  
+
   def start_new_game
     new_game = game.start_game
     
     puts display_welcome_message
     puts display_clue
     puts "Lives remaining: #{new_game.lives_remaining}"
-    
+
     play_turn
   end
-  
+
   def play_turn
     loop do
       turn = game.play_turn(guessed_letter)
       if display_progress(turn) == true then break; end
     end
   end
-  
+
   def display_welcome_message
     "Welcome to Hangman"
   end
-  
+
   def display_progress(turn)
     puts turn.guess_result
     puts turn.lives_remaining
     puts display_clue
     puts display_guesses(turn.guesses)
-    
+
     turn.game_over?
   end
-  
+
   def display_clue
     new_game = game.start_game
     place_holder = "_"
     clue = new_game.clue.map { |e| e == nil ? place_holder : e }
     "Your clue: #{clue.join(" ")}"
   end
-  
+
   def display_guesses(guesses)
     "Previous guesses: #{guesses.join(" ")}"
   end
-  
+
   def ask_for_guess
     "Please make a guess:"
   end
-  
+
   def guessed_letter
     gets.chomp
   end
--- spec/console_ui_spec.rb
+++ spec/console_ui_spec.rb
@@ -3,24 +3,24 @@ require "byebug"
 
 RSpec.describe ConsoleUI do
   subject (:console_ui) { described_class.new }
-  
+
   describe "#display_welcome_message" do
     it "creates welcome message at start of game" do
       expect(console_ui.display_welcome_message).to eq "Welcome to Hangman"
     end
   end
-  
+
   describe "#display_clue" do
     context "when game first starts" do
       it "returns a masked word" do
         new_game = console_ui.game
         clue = new_game.send(:word).chars.map! { |element| element = "_" }.join(" ")
-        
+
         expect(console_ui.display_clue).to eq "Your clue: #{clue}"
       end
     end
   end
-  
+
   describe "#display_guesses" do
     context "when the first guess is made" do
       guesses = ["a"]
@@ -28,7 +28,7 @@ RSpec.describe ConsoleUI do
         expect(console_ui.display_guesses(guesses)).to eq "Previous guesses: a"
       end
     end
-    
+
     context "when a second unique guess is made" do
       guesses = ["a", "b"]
       it "displays a single guess" do
